9da09f6e8facd0327f55141a7c1f36843d87029c
🌌 <Time.Rain.Star.Sky@Gmail.com>
2025-08-23 19:32:38 +0800

    添加 事件钩子🪝

--- src/app/repeater.ts
+++ src/app/repeater.ts
@@ -2,8 +2,12 @@ import { createPlugin } from "philia/plugin"
 
 export const repeater = new createPlugin("复读机", "发送 /复读 加内容来复读。").command(
   "复读",
-  (ctx, args) => {
+  async (ctx, args) => {
+    if (!args) {
+      await ctx.reply("请发送要复读的内容")
+      args = (await ctx.hook.promise()).event.summary
+    }
     ctx.logger.info(ctx.event.user.id, "复读", args)
-    ctx.reply(args)
+    await ctx.reply(args)
   },
 )
